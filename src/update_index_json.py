84ecb1796a00d9e3d79002298b49f5e494ce7aac
Alla Doroshkevych <alla.doroshkevych@oracle.com>
2022-12-13 16:35:07 -0500

    test fix

--- src/update_index_json.py
+++ src/update_index_json.py
@@ -118,11 +118,14 @@ def parse_notebooks(uri: str, block_size:int, auth: dict = None, parse_by_line:b
     while i < execute_times:
         i+= 1
         auth = auth or {}
+        print("uri", uri)
         scheme = urlparse(uri).scheme or "file"
+        print("scheme", scheme)
+        dir_content = file_system.ls(uri)
         
         file_system = fsspec.filesystem(scheme, **auth)
         files = [
-            file for file in file_system.ls(uri) #if file.endswith(".ipynb")
+            file for file in dir_content #if file.endswith(".ipynb")
         ]
         print("files", files)
         method = parse_file if parse_by_line else parse_nb
