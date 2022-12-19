f0cfc24dd92bb7ed2b39364cb295a2c6cde4b1fb
Alla Doroshkevych <alla.doroshkevych@oracle.com>
2022-12-19 12:30:16 -0500

    no sorting

--- notebook_examples/make_readme.py
+++ notebook_examples/make_readme.py
@@ -143,6 +143,7 @@ The ADS SDK can be downloaded from [PyPi](https://pypi.org/project/oracle-ads/),
 
     # sort by create_time and write to index.json, newest first
     # index_json_content.sort(key=lambda nb: nb['create_time'], reverse=True)
+    
     with open(INDEX_FILE, "w") as index_file:
         json.dump(index_json_content, index_file, sort_keys=True, indent=4)
 
